100881929d6daaacf6a248aa382cdb1af9d86351
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-09-15 22:41:26 -0300

    feat: create user

---
+++ src/modules/backoffice/schemas/user.schema.ts
@@ -0,0 +1,40 @@
+import * as mongoose from 'mongoose'
+
+export const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    index: {
+      unique: true,
+    }
+  },
+
+  email: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  roles: [
+    {
+      type: String,
+      required: true,
+      enum: ['user', 'admin'],
+      default: 'user'
+    }
+  ],
+
+  active: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+  
+})
